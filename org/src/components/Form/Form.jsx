70e14def89b3af6b4d5f001fe4fb2e4c85121799
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-10 13:53:47 -0500

    use event for manage the re charge page

--- org/src/components/Form/Form.jsx
+++ org/src/components/Form/Form.jsx
@@ -4,9 +4,15 @@ import OptionList from "../OptionList/OptionList";
 import Button from "../Button/Button";
 
 function Form() {
+
+    const sendManagement = (event) => {
+        event.preventDefault()
+        console.log("Manejar el envio", event)
+    }
+
     return (
         <section className="form-usr">
-            < form >
+            < form onSubmit={sendManagement}>
                 <h2>Rellena el formulario para crear el colaborador.</h2>
                 <TextField input_label="Nombre" placeholder="Ingresar nombre" />
                 <TextField input_label="Puesto" placeholder="Ingresar puesto" />
