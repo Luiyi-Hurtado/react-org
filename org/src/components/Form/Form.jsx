e3a7bc29cd209abcff57d30623c5fb87c483196e
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-10 10:44:02 -0500

    Create TextField component, upadre Form component. Create the TextFiel component for use in the Form

--- org/src/components/Form/Form.jsx
+++ org/src/components/Form/Form.jsx
@@ -1,10 +1,14 @@
 import "./Form.css"
+import TextField from "../TextField/TextField";
 
 function Form() {
     return (
         <section className="form-usr">
             < form >
                 <h2>Rellena el formulario para crear el colaborador.</h2>
+                <TextField />
+                <TextField />
+                <TextField />
             </form >
         </section >
     );
---
+++ org/src/components/TextField/TextField.jsx
@@ -0,0 +1,12 @@
+import './TextField.css'
+
+function TextField() {
+    return (
+        <div className='text-field'>
+            <label>Nombre</label>
+            <input placeholder='Ingresar nombre' />
+        </div>
+    );
+}
+
+export default TextField
