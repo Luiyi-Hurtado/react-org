6e4d73669494f342219c35ef35c63fbe7d4f41eb
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-11 10:02:46 -0500

    use map and destructaration. use the map for iterate in the teams array and use this data in the team title. use the desctructuration for create clean code.

--- org/src/App.js
+++ org/src/App.js
@@ -12,19 +12,51 @@ function App() {
     updateShowForm(!showForm);
   }
 
+  const teams = [
+    {
+      title: "Programación",
+      primaryColor: "#57C278",
+      secondaryColor: "#D9F7E9"
+    },
+    {
+      title: "Front-End",
+      primaryColor: "#82CFFA",
+      secondaryColor: "#E8F8FF"
+    },
+    {
+      title: "Data Science",
+      primaryColor: "#A6D157",
+      secondaryColor: "#F0F8E2"
+    },
+    {
+      title: "DevOps",
+      primaryColor: "#E06B69",
+      secondaryColor: "#FDE7E8"
+    },
+    {
+      title: "UX Y Diseño",
+      primaryColor: "#DB6EBF",
+      secondaryColor: "#FAE9F5"
+    },
+    {
+      title: "Móvil",
+      primaryColor: "#FFBA05",
+      secondaryColor: "#FFF5D9"
+    },
+    {
+      title: "Innovación y Gestión",
+      primaryColor: "#FF8A29",
+      secondaryColor: "#FFEEDF"
+    }
+  ];
+
   return (
     <div>
       <Header />
       {showForm ? <Form /> : <></>}
       {/* {showForm && <Form />} */}
       <MyOrg changeShow={changeShow} />
-      <Team team="Programación" />
-      <Team team="Front-End" />
-      <Team team="Data Science" />
-      <Team team="DeOps" />
-      <Team team="UX y Diseño" />
-      <Team team="Móvil" />
-      <Team team="Innovación y Gestión" />
+      {teams.map((team, index) => <Team teamData={team} key={index} />)}
     </div>
   );
 }
--- org/src/components/Team/Team.jsx
+++ org/src/components/Team/Team.jsx
@@ -1,9 +1,14 @@
 import "./Team.css";
 
 const Team = (props) => {
+
+    const { title, primaryColor, secondaryColor } = props.teamData
+    const styleBackground = { backgroundColor: secondaryColor }
+    const styleTitle = { borderBottomColor: primaryColor }
+
     return (
-        <section className="team">
-            <h3>{props.team}</h3>
+        <section className="team" style={styleBackground}>
+            <h3 style={styleTitle}>{title}</h3>
             <div className="team-colaborator">
             </div>
         </section>
