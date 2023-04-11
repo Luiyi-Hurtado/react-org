bde5b4b5a8d3d6ebd5247ff8adc563d9df787904
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-10 20:53:50 -0500

    use the hook useState for a simple examples

--- org/src/components/MyOrg/MyOrg.jsx
+++ org/src/components/MyOrg/MyOrg.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./MyOrg.css";
 
 function MyOrg() {
+
+    const [show, updateShow] = useState(true)
+
+    function clickManagenet() {
+        console.log("Mostrar/Ocultar Elemento", !show)
+        updateShow(!show)
+    }
+
     return (
         <section className="org-section">
             <h3 className="org-title">Mi Organización</h3>
-            <img src="/img/add.png" alt="add" />
+            <img src="/img/add.png" alt="add" onClick={clickManagenet} />
         </section>
     );
 }
